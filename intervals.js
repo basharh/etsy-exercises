9a8001de9d3a1ec69f4d6e22833cd7c53292ff49
Bashar Harfoush <bashar.harfoush@gmail.com>
2019-03-27 18:51:45 -0400

    print most visited position

--- intervals.js
+++ intervals.js
@@ -1,13 +1,12 @@
 
-
 function getMostVisited(n, sprints) {
   if ( !Array.isArray(sprints) || typeof n !== 'number' || sprints.length < 2 || n < 2 )
     throw "Invalid Arguments";
 
-  let position = new Map();
+  let position_visited = new Map();
 
   for ( let i = 1; i <= n; i++ ) {
-    position.set(i, 0); // initialize visits
+    position_visited.set(i, 0); // initialize visits
   }
 
   for ( let i = 0; i < sprints.length - 1; i++ ) {
@@ -15,12 +14,22 @@ function getMostVisited(n, sprints) {
     let end = sprints[i] < sprints[i+1] ? sprints[i+1] : sprints[i];
 
     for ( let j = start; j <= end; j++ ) {
-      let visited = position.get(j);
-      position.set(j, visited + 1)
+      let visited = position_visited.get(j);
+      position_visited.set(j, visited + 1)
     }
   }
 
-  console.log(position);
+  console.log(position_visited);
+
+  let positions = Array.from(position_visited.keys());
+
+  // sort positions by number of visited
+  positions.sort( (a, b) => {
+    let sort = position_visited.get(a) - position_visited.get(b);
+    return sort === 0 ? b - a : sort;
+  });
+
+  return positions.pop();
 }
 
 getMostVisited( 5, [ 2, 4, 1, 3] );
